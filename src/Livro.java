7bddf846ec185d1610a77147f9c79c3f9ccf8eb2
chrisverbarbuto <chrisvbarbuto@gmail.com>
2026-03-12 09:45:19 -0300

    Classe Livro e criação da classe teste com o objeto meuLivro

---
+++ src/Livro.java
@@ -0,0 +1,19 @@
+public class Livro {
+    //Tipos primitivos: int, double, char, boolean
+    //Atributos: Caracteristicas do objeto
+    //tipo nomeAtributo
+
+    String titulo;
+
+    String autor;
+
+    double preco;
+
+    String editora;
+
+    int paginas;
+
+    String resumo;
+
+    String tipoCapa;
+}
---
+++ src/TesteLivro.java
@@ -0,0 +1,10 @@
+public class TesteLivro {
+    public static void main(String[] args) {
+        //Criar objetos - instanciar
+        Livro meuLivro = new Livro();
+        meuLivro.titulo = "Harry Potter";
+        meuLivro.editora = "editora";
+
+    }
+
+}
